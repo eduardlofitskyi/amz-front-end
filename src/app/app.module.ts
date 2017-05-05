d5639104bedcd675b6757743e94774d437a9841f
lofitskyi <eduard.lofitskyi@nixsolutions.com>
2017-05-05 18:59:15 +0300

    add support smart-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { AppComponent } from './app.component';
+import {BasicExampleDataComponent} from "./components/smart_table/smart-table.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicExampleDataComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2SmartTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
